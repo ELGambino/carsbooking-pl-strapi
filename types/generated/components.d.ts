7cca66cbed7260ac443130842626dc9e33823941
ELGambino <influencebusinesspl@gmail.com>
2024-06-13 12:53:52 +0200

    commit to main

---
+++ types/generated/components.d.ts
@@ -0,0 +1,34 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface CarsFeature extends Schema.Component {
+  collectionName: 'components_cars_features';
+  info: {
+    displayName: 'Feature';
+    icon: 'magic';
+    description: '';
+  };
+  attributes: {
+    Name: Attribute.String;
+  };
+}
+
+export interface RentalsAdditionalServices extends Schema.Component {
+  collectionName: 'components_rentals_additional_services';
+  info: {
+    displayName: 'AdditionalServices';
+    icon: 'plus';
+  };
+  attributes: {
+    Name: Attribute.String;
+    Price: Attribute.Integer;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'cars.feature': CarsFeature;
+      'rentals.additional-services': RentalsAdditionalServices;
+    }
+  }
+}
